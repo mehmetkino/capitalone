bc79f5871f5f0eb426ecd8c25c16c2e7c4aceb8b
mehmetkino <mehmetkino@yahoo.com>
2023-03-18 18:21:20 -0400

    this is .gitignore

--- Test/src/test2/hello.java
+++ Test/src/test2/hello.java
@@ -3,5 +3,7 @@ package test2;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hi this is java");
+        System.out.println("Hi this is my .gitignore");
+
     }
 }
