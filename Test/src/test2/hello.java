e64974b2689238b5d34cd4e2bd8c8de75698a4a7
mehmetkino <mehmetkino@yahoo.com>
2023-03-18 18:30:48 -0400

    this is new branch with gitignore

--- Test/src/test2/hello.java
+++
@@ -1,9 +0,0 @@
-package test2;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("hi this is java");
-        System.out.println("Hi this is my .gitignore");
-
-    }
-}
